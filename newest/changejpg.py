adea5666196f0540f9142e54e12e718cd32e92e3
King <13321998692@163.com>
2021-09-14 13:21:36 +0800

    newest

--- newest/changejpg.py
+++ newest/changejpg.py
@@ -5,14 +5,15 @@ import os
 def renaming(file):
     """修改后缀"""
 
-    if(file.find("123")!=-1):
-        #有问题
-        print(file)
     ext = os.path.splitext(file)    # 将文件名路径与后缀名分开
  
     if ext[1] != '.jpg' and ext[1] != '.md':                    # 文件名：ext[0]
         new_name = ext[0] + '.jpg'         # 文件后缀：ext[1]
-        os.rename(file, new_name)
+        try:
+            os.rename(file, new_name)
+        except:
+            print("error")
+            os.remove(file)
         print(file)
  
  
@@ -26,9 +27,9 @@ def tree(path):
         else:
             os.chdir(path)      # 修改工作地址（相当于文件指针到指定文件目录地址）
             renaming(file)      # 修改后缀
- 
-try:
-    tree("D:\\Project\\cjhbUpdate\\newest\\downloadserver")
-except Exception as e:
-    print(e)
-    pass
+while 1:
+    try:
+        tree("D:\\Project\\cjhbUpdate\\newest\\downloadserver")
+    except Exception as e:
+        print(e)
+        pass
--- newest/downphoto.py
+++ newest/downphoto.py
@@ -9,22 +9,38 @@ def IDMdownload(DownUrl, DownPath, FileName):
     IDM = "IDMan.exe"
     command = ' '.join([IDM, '/d', DownUrl, '/p', DownPath, '/f', FileName, '/a', '/d'])
     os.system(command)
-bar = Bar('Processing', max=10000,
+bar = Bar('Processing', max=13000,
           suffix='%(index)d/%(max)d - %(percent).2f%% - [%(elapsed)ds<%(eta)ds]')
 full=0
 for root, dirs, files in os.walk('D:\\Project\\cjhbUpdate\\newest\\downloadserver'):
     #print(root)
     #print(files)
+    
     if(len(root)!=0 and len(files)!=0):
-        filedir=root+'\\'+files[0]
-        file = open(filedir, 'r',encoding='utf-8',errors='ignore')
-        data = file.read()
-        reg = r'!\[\]\((.*?)\)'
-        ia = re.compile(reg)
-        datade=data
-        sat = re.findall(ia, datade)
-        for x in sat:
-            print(x)
+        for xs in files:
+            if(xs[-2:]=='md'):
+                filedir=root+'\\'+xs
+                file = open(filedir, 'rb')
+                data = file.read()
+                reg = r'!\[\]\((.*?)\)'
+                ia = re.compile(reg)
+                datade=data.decode(encoding='utf-8',errors='ignore')
+                sat = re.findall(ia, datade)
+                for x in sat:
+                    #print(x)
+                    #print(root,str(x))
+                    reg2 = r'sign=.*?\/(.*?jpg)'
+                    ia2 = re.compile(reg2)
+                    sat2 = re.findall(ia2, x)
+                    if(os.path.exists(root+'\\'+sat2[0])):
+                        bar.next()
+                        continue
+                    doen='http://tiebapic.baidu.com/forum/pic/item/'+sat2[0]
+                    #print(root)
+                    #二次下载后处理异常
+                    print(x)
+                    #IDMdownload(x,root,sat2[0])
+                    bar.next()
     else:
         print(root)
 bar.finish
